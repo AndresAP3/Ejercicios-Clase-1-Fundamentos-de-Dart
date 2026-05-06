b58d1b479ac59bc655d3c42cf7517fd23764b3f3
Andres <anfe3101ap@gmail.com>
2026-05-05 19:16:01 -0500

    Completando ejercicios de fundamentos de Dart

--- Presentación con interpolación.dart
+++ Presentación con interpolación.dart
@@ -1,6 +1,6 @@
 void main() {
-  String nombre = 'Andres';
-  int edad = 22; 
+  String nombre = 'Andrés';
+  int edad = 25; 
   String ciudad = 'Bucaramanga';
 
   print('Hola, soy $nombre, tengo $edad años y soy de $ciudad.');
